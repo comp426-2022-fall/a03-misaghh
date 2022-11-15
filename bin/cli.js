975782e49bfed2ce806b665336f01de9d10b804d
Misagh  Haghar <misaghh@email.unc.edu>
2022-11-15 13:27:08 -0500

    added the default values for dice, side, rolls

--- bin/cli.js
+++ bin/cli.js
@@ -1,2 +1,11 @@
 import minimist from "minimist";
-import { roll } from "/lib/roll.js"
+import { roll } from "/lib/roll.js";
+
+
+const args = minimist(process.argv.slice(2));
+
+
+const sides = args.sides || 6;
+const dice = args.dice || 2;
+const rolls = args.rolls || 1;
+
