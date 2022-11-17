bf4a6c5841d1ef756ecb998ddb80e3f33448de0e
Misagh  Haghar <misaghh@email.unc.edu>
2022-11-16 23:21:54 -0500

    maybe done?

--- bin/cli.js
+++ bin/cli.js
@@ -10,3 +10,5 @@ const dice = args.dice || 2;
 const rolls = args.rolls || 1;
 
 let output = roll(side, dice, rolls);
+
+console.log(JSON.stringify({"side": sides, "dice": dice, "roll": rolls, "output": output}));
